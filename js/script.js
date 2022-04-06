4c54301601b3e9a069dd079dd24e427ec485c00c
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2022-04-06 13:04:40 -0300

    alterado caminho api

--- js/script.js
+++ js/script.js
@@ -44,5 +44,5 @@ funcionamento.init();
 const animacaoScroll = new AnimacaoScroll('[data-anime="scroll"]');
 animacaoScroll.init();
 
-fetchAnimais("../../animaisapi.json", ".numeros-grid");
+fetchAnimais("./animaisapi.json", ".numeros-grid");
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
